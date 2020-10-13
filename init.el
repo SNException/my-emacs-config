bce573276e43460bf964308e60460d9362747c24
SNException <thexsalkin@gmail.com>
2020-10-13 15:29:15 +0200

    update

--- init.el
+++ init.el
@@ -10,16 +10,12 @@
 (defun display-startup-echo-area-message ()
   (message ""))
 
+(remove-hook 'find-file-hooks 'vc-refresh-state)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
-(setq backup-directory-alist '(("." . "~/.emacs_saves")))
-
-(remove-hook 'find-file-hooks 'vc-refresh-state)
 (setq scroll-error-top-bottom t)
 (setq-default indent-tabs-mode nil)
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(add-to-list 'load-path              "~/.emacs.d/themes/")
 (custom-set-faces
  '(default ((t (:family "Ubuntu Mono" :foundry "outline" :slant normal :weight normal :height 160 :width normal)))))
 (add-to-list 'default-frame-alist '(background-color . "#cacaca"))
