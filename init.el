15642a9e8068fc97238fac11c22ce909592ccc39
SNException <thexsalkin@gmail.com>
2020-10-14 17:27:24 +0200

    update

--- init.el
+++ init.el
@@ -1,9 +1,6 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(fringe-mode -1)
-(show-paren-mode)
-(setq column-number-mode t)
 
 (setq inhibit-startup-screen t)
 (setq inhibit-startup-message t)
@@ -13,17 +10,21 @@
 
 (remove-hook 'find-file-hooks 'vc-refresh-state)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+(show-paren-mode)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (setq scroll-error-top-bottom t)
+(setq column-number-mode t)
 (setq-default indent-tabs-mode nil)
 
-(custom-set-faces
- '(default ((t (:family "Ubuntu Mono" :foundry "outline" :slant normal :weight normal :height 195 :width normal))))
-)
-
+(define-key global-map (kbd "M-q") 'query-replace)
 (define-key global-map (kbd "C-S-f") 'forward-word)
 (define-key global-map (kbd "C-S-b") 'backward-word)
 (define-key global-map (kbd "C-S-v") 'scroll-down)
 (global-set-key "\M-n" "\C-u1\C-v")
 (global-set-key "\M-p" "\C-u1\M-v")
+
+(custom-set-faces
+ '(default ((t (:family "Ubuntu Mono" :foundry "outline" :slant normal :weight normal :height 195 :width normal))))
+)
+
